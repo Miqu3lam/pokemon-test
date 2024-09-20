1cab53efe963b4d5c27b29dbfe1382aedb97a160
Miqu3lam <167946317+Miqu3lam@users.noreply.github.com>
2024-09-20 12:42:34 -0300

    Update dode.js

--- dode.js
+++ dode.js
@@ -1,29 +1,39 @@
-var imagen = document.getElementById("img");
-var nome = document.getElementById("nome");
-var btn = document.getElementById("btn")
-var peso = document.getElementById("peso")
-var poke = document.getElementById("poke")
-var habi = document.getElementById("habi")
-var altura = document.getElementById("altura")
-var id = document.getElementById("id")
-var pokedex = document.getElementById("pokedex")
+let id = 1;
 
-
-
-document.getElementById("btn").addEventListener('click', () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${nome.value}/`, {
-        method: "GET"
-    })
-    .then(result => result.json())
+function buscarPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(response => response.json())
     .then(data => {
-        imagen.src = data.sprites.front_default;
-        peso.innerText = data.weight;
-        poke.innerText = data.name;
-        altura.innerHTML = data.height
-        id.innerHTML = data.id
-        pokedex.innerHTML = data.order
-        
+        document.getElementById("imagem").src = data.sprites.front_default;
+        document.getElementById("peso").innerText = data.weight;
+        document.getElementById("altura").innerText = data.height;
+        document.getElementById("poke").innerText = data.name;
+        document.getElementById("id").innerText = data.id;
+        document.getElementById("pokedex").innerText = data.order;
     })
     .catch(error => console.log(error));
+}
+
+document.getElementById("btnAvancar").addEventListener('click', () => {
+    id++;  
+    buscarPokemon(id);  
 });
 
+document.getElementById("btnVoltar").addEventListener('click', () => {
+    if (id > 1) {  
+        id--;
+        buscarPokemon(id);  
+    }
+});
+
+document.getElementById("buscar").addEventListener('input', (event) => {
+    const inputValue = event.target.value;
+    id = inputValue
+    
+    if (inputValue) {
+        buscarPokemon(inputValue);  
+    }
+});
+
+
+buscarPokemon(id);
